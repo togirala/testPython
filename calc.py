8267c098a2adac2a8c02da5af15067166585e26a
Teja Ogirala <teja90.exe@gmail.com>
2017-11-20 18:38:55 -0800

    First Test

---
+++ calc.py
@@ -0,0 +1,12 @@
+
+def add(val1, val2):
+    return val1 + val2
+
+def subract(val1, val2):
+    return val1 - val2
+
+def multiply(val1, val2):
+    return val1 * val2
+
+def divide(val1, val2):
+    return val1 / val2
---
+++ test_calc.py
@@ -0,0 +1,13 @@
+import calc
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+class test_calc(self):
+    
+    
+    def test_add(self):
+        self.assertEqual(calc.add(10, 5), 15)
+        
